2c77d85f462b3629d869e1b5be80152ac2a73963
1janisha <1janisha@gmail.com>
2024-02-18 15:26:20 +0000

    Rook cannot move through friendly piece

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -7,16 +7,37 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        let location = board.findPiece(this); 
+        let location = board.findPiece(this);
+        const row = location.row;
+        const col = location.col;
         const moves = [];
+        const directions = [
+            {row: 1, col: 0},
+            {row: -1, col: 0},
+            {row: 0, col: 1},
+            {row: 0, col:-1}
+        ];
 
-        for( let i = 0; i <= 7; i++ ) {
-            if (i !== location.row) {
-                moves.push(Square.at(i, location.col));
+        for(let direction of directions) {
+            for(let i=1; i<8; i++) {
+                const potentialSquare = Square.at(row + direction.row * i, col + direction.col * i);
+                // Check square is on the board
+                if(potentialSquare.row > 7 || potentialSquare.row < 0 || potentialSquare.col > 7 || potentialSquare.col < 0) {
+                    break;
+                }
+                const piece = board.getPiece(potentialSquare);
+                // Check for empty square
+                if(piece === undefined) {
+                    moves.push(potentialSquare);
+                    continue;
+                }
+                // Check if opposing player
+                if(piece.player !== this.player) {
+                    moves.push(potentialSquare);
+                }
+                // Have either come across opposing or own piece
+                break;
             }
-            if ( i !== location.col) {
-                moves.push(Square.at(location.row, i));
-            }           
         }
         return moves;
     }

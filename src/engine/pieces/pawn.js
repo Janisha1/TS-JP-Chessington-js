b423c70898c77115f1dee788e8e0a3d2595bd523
1janisha <1janisha@gmail.com>
2024-02-18 14:58:40 +0000

    Red-18: Pawn can take opposition piece (not King) if diagonal move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,6 +1,7 @@
 import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
+import King from './king';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -9,18 +10,35 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this);
+        let row = location.row;
+        let col = location.col;
         const moves = [];
         let direction = this.player === Player.WHITE ? 1 : -1;
         let startPosition = this.player === Player.WHITE ? 1 : 6;
-        let squareAhead = Square.at(location.row + direction, location.col);
-        let squareTwoAhead = Square.at(location.row + direction * 2, location.col);
-
+        let squareAhead = Square.at(row + direction, col);
+        let squareTwoAhead = Square.at(row + direction * 2, col);
+        // Check for potential moves ahead
         if(!board.getPiece(squareAhead)) {
             moves.push(squareAhead);
-            if(location.row === startPosition) {
+            if(row === startPosition) {
                 if(!board.getPiece(squareTwoAhead)) {
                     moves.push(squareTwoAhead);
                 }
+            }
+        }
+        // Check for potential moves diagonally - if opposing player piece is on that square
+        if(col !== 0) {
+            const diagonalMove = Square.at(location.row + direction, location.col - 1);
+            const potentialPiece = board.getPiece(diagonalMove);
+            if(potentialPiece && (potentialPiece.player !== this.player) && !(potentialPiece instanceof King)) {
+                moves.push(diagonalMove);
+            }
+        }
+        if(col != 7) {
+            const diagonalMove = Square.at(location.row + direction, location.col + 1);
+            const potentialPiece = board.getPiece(diagonalMove);
+            if(potentialPiece && (potentialPiece.player !== this.player) && !(potentialPiece instanceof King)) {
+                moves.push(diagonalMove);
             }
         }
         return moves;
